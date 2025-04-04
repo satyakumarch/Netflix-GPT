86cbb4e7251174051f3409b7d8c17a8837c61109
Satyakumarch <satyakumarchaudhary603@gmail.com>
2025-04-05 04:34:02 +0530

    Frontend part of the login page is completed

--- netflix-gpt/src/App.js
+++ netflix-gpt/src/App.js
@@ -1,12 +1,8 @@
-import './App.css';
+import Body from './Component/Body';
 
 function App() {
   return (
-    <div className="App">
-    <h1 className="text-3xl font-bold underline col-span-3 bg-zinc-400 border-spacing-3 border-2 border-red-500">
-      Hello world!
-    </h1>
-    </div>
+    <Body />
   );
 }
 
